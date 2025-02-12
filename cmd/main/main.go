9372cbd7f6a513d4dd4441bb65b721f56783b10c
Лёва Нерсесян <lyova123454321@gmail.com>
2025-02-12 07:05:11 +0300

    надеюсь на название коммитов вы смотреть не будете, а то тут по приколу всё

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,111 +1,3 @@
-// package main
-
-// import (
-// 	"context"
-// 	"fmt"
-// 	"net/http"
-// 	"os"
-// 	"os/signal"
-// 	"syscall"
-// 	"time"
-
-// 	shortener "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener"
-// 	shortenerDelivery "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/delivery/http"
-// 	inmemoryRepo "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/repo/inmemory"
-// 	postgresRepo "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/repo/postgres"
-// 	shortenerUsecase "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/usecase"
-
-// 	"github.com/gorilla/mux"
-// 	"github.com/jackc/pgx/v4"
-// 	"github.com/jackc/pgx/v4/pgxpool"
-// 	"github.com/joho/godotenv"
-// )
-
-// func init() {
-// 	if err := godotenv.Load(); err != nil {
-// 		fmt.Println("Error loading .env:", err)
-// 	}
-// }
-
-// func main() {
-// 	var repoInstance shortener.ShortenerRepo
-// 	var closeFunc func()
-
-// 	storageType := os.Getenv("STORAGE")
-// 	if storageType == "inmemory" {
-// 		fmt.Println("Using In-Memory storage")
-// 		repoInstance = inmemoryRepo.NewInMemoryRepo()
-// 		closeFunc = func() {}
-// 	} else {
-// 		fmt.Println("Using PostgreSQL storage")
-// 		db, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-// 		if err != nil {
-// 			fmt.Println("Error connecting to database:", err)
-// 			return
-// 		}
-// 		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-// 		if err != nil {
-// 			fmt.Println("Error connecting to database:", err)
-// 			db.Close()
-// 			return
-// 		}
-// 		repoInstance = postgresRepo.NewShortenerRepo(db, *conn)
-// 		closeFunc = func() {
-// 			db.Close()
-// 		}
-// 	}
-// 	defer closeFunc()
-
-// 	// Инициализируем Usecase и HTTP Delivery
-// 	uc := shortenerUsecase.NewShortenerUsecase(repoInstance)
-// 	hd := shortenerDelivery.NewShortenerHandler(uc)
-
-// 	r := mux.NewRouter().PathPrefix("/api").Subrouter()
-// 	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Println("not found")
-// 	})
-// 	http.Handle("/", r)
-
-// 	// Настройка эндпоинтов
-// 	shortenerRouter := r.PathPrefix("/v1").Subrouter()
-// 	{
-// 		shortenerRouter.Handle("/create", http.HandlerFunc(hd.CreateAndSaveShortLink)).
-// 			Methods(http.MethodPost, http.MethodOptions)
-// 		shortenerRouter.Handle("/get", http.HandlerFunc(hd.GetShortLink)).
-// 			Methods(http.MethodGet, http.MethodOptions)
-// 	}
-
-// 	// Настройка graceful shutdown
-// 	signalCh := make(chan os.Signal, 1)
-// 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
-
-// 	server := &http.Server{
-// 		Handler:           r,
-// 		Addr:              ":8080",
-// 		ReadTimeout:       10 * time.Second,
-// 		WriteTimeout:      10 * time.Second,
-// 		ReadHeaderTimeout: 10 * time.Second,
-// 	}
-
-// 	go func() {
-// 		if err := server.ListenAndServe(); err != nil {
-// 			fmt.Println("Server Stopped:", err)
-// 		}
-// 	}()
-// 	fmt.Println("Server started on :8080")
-
-// 	<-signalCh
-// 	fmt.Println("Shutdown signal received")
-
-// 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-// 	defer cancel()
-
-// 	if err := server.Shutdown(ctx); err != nil {
-// 		fmt.Println("Server shutdown failed:", err)
-// 	}
-// }
-
 package main
 
 import (
@@ -125,14 +17,12 @@ import (
 	postgresRepo "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/repo/postgres"
 	shortenerUsecase "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/usecase"
 
+	shortenerpb "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/delivery/grpc/shortenerpb"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
-
-	// Импорт сгенерированного кода от proto
-	shortenerpb "github.com/Snake1-1eyes/Test_Ozon/internal/pkg/shortener/delivery/grpc/shortenerpb"
 )
 
 func init() {
@@ -198,7 +88,7 @@ func main() {
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 
-	// Создание и запуск gRPC сервера
+	// Создание и запуск GRPC сервера
 	lis, err := net.Listen("tcp", ":9090")
 	if err != nil {
 		fmt.Println("Failed to start gRPC listener:", err)
@@ -220,7 +110,7 @@ func main() {
 	}()
 	fmt.Println("HTTP server started on :8080")
 
-	// Запуск gRPC сервера
+	// Запуск GRPC сервера
 	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
 			fmt.Println("gRPC Server Stopped:", err)
